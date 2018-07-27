6bdae7eef5ab105ee027a878c413c5fa1afa9189
tlsvie <hirenzhao@hotmail.com>
2018-07-27 19:14:42 +0800

    Revert "Initial commit"
    This reverts commit 2a7143258a4038bd1e762bdb87252372783aba36.

--- mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from mysite import views
-
-
-urlpatterns = [
-    url(r'^about/$', views.about),
-    url(r'^list/(?P<list_date>\d{4}/\d{1,2}/\d{1,2})$', views.listing),
-    url(r'^$', views.index),
-]
